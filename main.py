ad6b66f19b1a0ed5115f19c089a00a0df320226b
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-01-18 21:19:14 +0200

    Added all addresses

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from threading import Timer
 from time import sleep
 
 api_key = '5055335035:AAG3s1i6tGyQhJyv0197Xl3j8CY9CAUzhDI'
+api_key_dev = "2121277949:AAGnsnht0fJVh_zrsybJdpuc9TgJn6YOo5c"
 pol_key = "SIZEEU48BVGR4U9UHWQ8S5DXT8N9IMMZ8V"
-add = "0x06761b0097c5f658bd368b453b330f7e26a5ea7e"
+add = ["0x06761b0097c5f658bd368b453b330f7e26a5ea7e","0xc5becc7e77144670de0c52896e87bd041e93d93e","0x74d1297985f921f08775451769add005dff1c1d3","0xd8f4c502b80d084bd53a91800947c5c0483f640f","0x06fe965cf71a8c4d9c1fc5865e2a82fff5b87ad9","0x7f8cb1b15bd8b405c7a2bd631ed323da72409f04","0x3bf228ba3fcbcc1ee7f367bfd2b8ecadb98cd8b4"]
 print("Bot running in tg")
+ShelfFile = shelve.open('shelf')
 
+for i in add:
+    ShelfFile[i]=''
+
+ShelfFile.close()
 updater = telegram.ext.Updater(api_key)
 disp = updater.dispatcher
-
-ShelfFile['merhi'] = ""
 
 class RepeatedTimer(object):
     def __init__(self, interval, function, *args, **kwargs):
@@ -42,30 +46,33 @@ class RepeatedTimer(object):
 
 def outputMessage(data):
     return '''
-    <b>Sohyoune l zaber</b>\nbought or minted this:\n\n<b>Contract address:</b> {}\n<b>NFT Name:</b> {}\n<b>URL:</b> {}\n 
-    '''.format(data['contractAddress'],data['tokenName'],'https://opensea.io/{}'.format(data['tokenName']))    
+    <b>{}</b>\nbought or minted this:\n\n<b>Contract address:</b> {}\n<b>NFT Name:</b> {}\n<b>URL:</b> {}\n 
+    '''.format(data['from'],data['contractAddress'],data['tokenName'],'https://opensea.io/{}'.format(data['tokenName']))    
 
 def printit (update,context):
-    print("running")
-    url = '''https://api.polygonscan.com/api?module=account&action=tokennfttx&address={}&startblock=0&endblock=99999999&page=1&sort=asc&apikey={}'''.format(add,pol_key)# print(url)
-    page = requests.get(url)
-    res = page.json()
-    results = res['result']
-    chosenData =results[len(results)-1]
-    ShelfFile = shelve.open('shelf')
+    message = ""
+    for i in add:
+        print("running")
+        ShelfFile = shelve.open('shelf')
+        url = '''https://api.polygonscan.com/api?module=account&action=tokennfttx&address={}&startblock=0&endblock=99999999&page=1&sort=asc&apikey={}'''.format(i,pol_key)# print(url)
+        page = requests.get(url)
+        res = page.json()
+        results = res['result']
+        chosenData =results[len(results)-1]
 
-    if(ShelfFile['merhi']!=chosenData['tokenID']):
-        ShelfFile['merhi']=chosenData['tokenID']
-        ShelfFile.close()
-        print(chosenData)
-        message = outputMessage(chosenData)
+        if(chosenData['tokenID'] not in ShelfFile[i]):
+            ShelfFile[i]+="-"+chosenData['tokenID']
+            ShelfFile.close()
+            message += outputMessage(chosenData)
+            message +="\n\n\n"
+            # real_balance = str("{0:.2f}".format(float(res['result'])/math.pow(10,18)))+" Matic"
+            # update.message.reply_text(real_balance)
+    if(len(message)>0):
         update.message.reply_text(message,parse_mode=ParseMode.HTML)
-        # real_balance = str("{0:.2f}".format(float(res['result'])/math.pow(10,18)))+" Matic"
-        # update.message.reply_text(real_balance)
-        
+
 def startList(update,context):
     update.message.reply_text("Fetching updates")
-    rt = RepeatedTimer(180.0, printit, update,context) # it auto-starts, no need of rt.start()
+    rt = RepeatedTimer(15.0, printit, update,context) # it auto-starts, no need of rt.start()
 
 disp.add_handler(telegram.ext.CommandHandler("start",startList))
 
